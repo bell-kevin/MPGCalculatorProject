36f06c2f3dbbbbe07a650d090b26cbd0f6c36bc8
Kevin Bell <bellKevin@pm.me>
2022-07-12 20:27:06 +0000

    Update MPG.java

--- src/mpgcalculator/MPG.java
+++ src/mpgcalculator/MPG.java
@@ -1,19 +1,16 @@
+/** ****************************************************************************
+ * MPG.java
+ * Kevin Bell
+ * This program is associated with MPGCalculator.java
+ ***************************************************************************** */
 package mpgcalculator;
 
-/******************************************************************************
-* MPG.java 
-* Programmer: @author jcboyd
-* Version: 2.0
-* Course: SDEV 2210
-* This program is associated with MPGCalculator.java 
-******************************************************************************/
-
 public class MPG {
-    
+
     private static double totalMiles, totalGallons, totalMPG;
     private static int numTrips = 0;
     private double miles, gallons, mpg;
-    
+
     public MPG(double miles, double gallons) {
         this.miles = miles;
         this.gallons = gallons;
@@ -23,31 +20,31 @@ public class MPG {
         this.mpg = this.miles / this.gallons;
         totalMPG = totalMiles / totalGallons;
     } //end MPG constructor
-    
+
     public double getMiles() {
         return this.miles;
     } //end getMiles
-    
+
     public double getGallons() {
         return this.gallons;
     } //end getGallons
-    
+
     public double getMPG() {
         return this.mpg;
     } //end getMPG
-    
+
     public double getTotalMiles() {
         return MPG.totalMiles;
     } //end getTotalMiles
-    
+
     public double getTotalGallons() {
         return MPG.totalGallons;
     } //end getTotalGallons
-    
+
     public int getNumTrips() {
         return MPG.numTrips;
     } //end getNumTrips
-        
+
     public double getTotalMPG() {
         return MPG.totalMPG;
     } //end getTotalMPG
@@ -55,11 +52,9 @@ public class MPG {
     public void displayCurrentMPG() {
         System.out.printf("This trip = %-5.2f MPG\n", this.mpg);
     } //end displayCurrentMPG
-    
+
     public static void displayTotalMPG() {
         System.out.printf("For %-1d trips, total MPG = %-6.2f\n",
                 numTrips, totalMPG);
-    } //end displayTotalMPG
-    
+    } //end displayTotalMPG 
 } //end class MPG
-
