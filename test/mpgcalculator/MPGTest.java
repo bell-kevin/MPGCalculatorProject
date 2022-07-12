41e9238e371ea37b12196a694051e215472b9b4e
Kevin Bell <bellKevin@pm.me>
2022-07-12 20:28:05 +0000

    Update MPGTest.java

--- test/mpgcalculator/MPGTest.java
+++ test/mpgcalculator/MPGTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MPGTest {
-        
+
     @Test
     public void testMPGMultiple() {
         //arrange
@@ -12,12 +12,11 @@ public class MPGTest {
         MPG second = new MPG(305, 16);
         MPG third = new MPG(325, 21);
         MPG fourth = new MPG(295, 14);
-
         //act and assert
         assertEquals(310, first.getMiles(), .00);
-        assertEquals(305, second.getMiles(), .00);       
-        assertEquals(325, third.getMiles(), .00);       
-        assertEquals(295, fourth.getMiles(), .00);       
+        assertEquals(305, second.getMiles(), .00);
+        assertEquals(325, third.getMiles(), .00);
+        assertEquals(295, fourth.getMiles(), .00);
         
         assertEquals(18, first.getGallons(), .00);
         assertEquals(16, second.getGallons(), .00);
@@ -28,13 +27,10 @@ public class MPGTest {
         assertEquals(19.06, second.getMPG(), .01);
         assertEquals(15.47, third.getMPG(), .01);
         assertEquals(21.07, fourth.getMPG(), .01);
-
+        
         assertEquals(4, first.getNumTrips());
         assertEquals(1235, second.getTotalMiles(), .00);
         assertEquals(69, third.getTotalGallons(), .00);
         assertEquals(17.89, fourth.getTotalMPG(), .01);
- 
-    } //end testMPGSingle
-    
+    } //end testMPGSingle    
 } //end class MPGTest
-
